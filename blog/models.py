f3b9c984867b2c073c1317a74e87671dd342957f
Dominic Yeboah <yeboahd24@gmail.com>
2022-03-21 14:43:37 -0400

    django gprc

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
